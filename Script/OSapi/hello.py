02c78796a3d508e71e0971766fb4f1722d372404
Minh Duong <duongtm.contact@gmail.com>
2023-09-26 16:14:26 +0700

    nt

--- Script/OSapi/hello.py
+++ Script/OSapi/hello.py
@@ -2,6 +2,7 @@ from keystoneauth1 import loading
 from keystoneauth1 import session
 from novaclient import client
 from neutronclient.v2_0 import client as nclient
+from cinderclient import client as cclient
 VERSION = "2.1"
 AUTH_URL = "http://116.103.227.31:5000"
 USERNAME = 'admin'
@@ -19,35 +20,52 @@ auth = loader.load_from_options(auth_url=AUTH_URL,
 sess = session.Session(auth=auth)
 nova = client.Client(VERSION, session=sess)
 neutron = nclient.Client(session=sess)
+cinder = cclient.Client(3, session=sess)
 servers = nova.servers.list()
 networks = neutron.list_networks()
+volumes = cinder.volumes.list()
 # ports = neutron.list_ports(network_id='263c3ed0-6a54-47f1-b2ac-abcd64cc2ffd')
-print(servers)
+print(volumes)
 # print(networks)
 # print(ports)
-nova.servers.create(name="testPy",
-                    image="3ebbfd02-969c-4e48-b995-d7e4058b9db2",
-                    flavor="2",
-                    nics=[{"net-id": "d2b875b3-88a2-4bf8-a38b-07cffdd4dbab"}])
 
+
+try:
+    volume1 = cinder.volumes.create(name="vol1",
+                                imageRef="462d75d9-026f-404d-a344-e5f584dd5172", 
+                                size = 1)
+    print(volume1.id)
+    volId=volume1.id
+    volBootable = False
+    while not volBootable:
+        volInfo = cinder.volumes.get(f"{volId}")
+        volBootable = False if volInfo.bootable == "false" else True
+    else:
+        vm1 = nova.servers.create(name="testCirros",
+                        image="",
+                        block_device_mapping_v2=[{'uuid': f'{volId}', 'boot_index': '0', 'source_type': 'volume', 'destination_type': 'volume'}],
+                        flavor="1",
+                        nics=[{"net-id": "cd907a3a-6b3a-46cb-b181-e7192fe5bfe7"}])
+        print(vm1.id)
+
+except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+# volume1 = cinder.volumes.create(name="testVol",
+#                                 imageRef="462d75d9-026f-404d-a344-e5f584dd5172", 
+#                                 size = 1)
+
+# print(volume1)
+# volume=
+# 'block_device_mapping_v2': [{'uuid': 'ea3497f7-d2cc-4a8e-af5a-f7fb675f1ac5', 'boot_index': '0', 'source_type': 'volume', 'destination_type': 'volume'}]
+# nova.servers.create(name="testPy",
 # vm1 = nova.servers.create(name="testCirros",
-#                     image="efde7b43-0c4e-4c95-b53e-6fe5b9e9eb2e",
+#                     image="",
+#                     block_device_mapping_v2=[{'uuid': '8c5626eb-98e1-4fa7-a978-e20823a5bde8', 'boot_index': '0', 'source_type': 'volume', 'destination_type': 'volume'}],
 #                     flavor="1",
-#                     nics=[{"net-id": "0ff6f2ab-fe02-4042-ad22-be7013f26383"}]
+#                     nics=[{"net-id": "cd907a3a-6b3a-46cb-b181-e7192fe5bfe7"}]
 #                     )
 # print(vm1.id)
 
-# vm1del=nova.servers.delete("522617f6-c353-4917-92b5-b2f0476756bb")
-# print(vm1del)
-
-# port = {'name': "port5",'network_id': "263c3ed0-6a54-47f1-b2ac-abcd64cc2ffd",'admin_state_up': True, 'port_security_enabled': False}
-# out = neutron.create_port({'port':port})
-# print(out['port']['id'])
-# print(out['port']['fixed_ips'][0]['ip_address'])
-
-# portInfo = 'port4'
-# outInfo = neutron.show_port(f'?name={portInfo}')
-# print(outInfo['ports'][0]['id'])
-# port = {'id': "da908eb4-d2f7-4393-958b-9287fb28962c"}
-# delPort = neutron.delete_port("da908eb4-d2f7-4393-958b-9287fb28962c")
-# print(delPort)
+# cinder.volumes.delete("8c5626eb-98e1-4fa7-a978-e20823a5bde8")
